00797a26f4493ae92039d7cb9d526620a49b121a
mhofer1976 <mhofer@1stquad.com>
2025-05-07 11:49:57 +0200

    Moved run_api.py into resource_planner package and updated launch configuration

--- src/resource_planner/run_api.py
+++ src/resource_planner/run_api.py
@@ -3,7 +3,7 @@
 Script to run the Resource Planner API server.
 """
 
-from src.api.resource_planner_api import app
+from .api.resource_planner_api import app
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000) 
